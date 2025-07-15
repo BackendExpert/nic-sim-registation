749caf7e67dfee798d6042a35ed12b3aae403b6a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-07-15 06:01:21 +0530

    Create SignIn.jsx

---
+++ client/src/pages/AuthPages/SignIn.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SignIn = () => {
+    return (
+        <div className='bg-gray-200'>
+            <div className="max-w-5xl mx-auto bg-white p-8">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, similique temporibus dicta alias illum repudiandae molestias ut voluptates dignissimos praesentium ipsum accusantium quam optio maiores, nulla exercitationem fugit a iste.
+            </div>
+        </div>
+    )
+}
+
+export default SignIn
